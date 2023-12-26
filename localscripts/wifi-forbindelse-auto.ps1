1788b4192eca5785aa641d951d1c21314e3a1f33
AnderSec <149626977+AnderSec@users.noreply.github.com>
2023-12-26 13:39:45 +0100

    Add files via upload

---
+++ localscripts/wifi-forbindelse-auto.ps1
@@ -0,0 +1,5 @@
+﻿# Importer wifi-profil fra USB-drev.
+netsh wlan add profile filename="{wifi xml filsti}" Interface="WI-FI" user=current
+# Forbinde til KFS-INTERN wifi
+netsh wlan connect ssid={Din SSID} name={Navn}
+
